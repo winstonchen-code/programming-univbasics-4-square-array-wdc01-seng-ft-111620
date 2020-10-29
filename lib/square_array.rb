078693889b39e8bcdc6b42aee129a18c40860b0b
Winston Chen <winstonchen.wm@gmail.com>
2020-10-29 04:11:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,9 @@
 def square_array(array)
   count = 0
   while count < array.length do 
-  array[count] = array[count] **2 
-  counter += 1
-end 
-square_array
+    array[count] = array[count] **2 
+    counter += 1
+  end
 end
 
 
